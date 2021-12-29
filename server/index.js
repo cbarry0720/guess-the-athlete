6b0cd1523e0c0ceed457135ef8e963df705961ea
cbarry0720 <chrisb0720@gmail.com>
2021-12-28 20:33:08 -0500

    almost done with scraping

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const nbaplayers = require('./routes/nbaplayers.js')
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(express.json());
+
+app.use("/nbaplayers", nbaplayers);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
